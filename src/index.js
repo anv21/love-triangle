1e74f3c2898d30840ec5182fa68526d6375f6784
anastasia <novakanastasya@yandex.ru>
2018-02-24 20:41:49 +0300

    The implementation of getLoveTrianglesCount.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,34 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  const foundLoveTriangles = [];
+
+  for (let i = 0; i < preferences.length; i++) {
+    if (preferences[i] !== null) {
+      for (let j = 0; j < preferences.length; j++) {
+        if (j !== i && preferences[j] !== null) {
+          for (let k = 0; k < preferences.length; k++) {
+            if (k !== i && k !== j && preferences[k] !== null) {
+
+              const isLoveTriangle = preferences[i] === j + 1 && preferences[j] === k + 1 && preferences[k] === i + 1;
+
+              if (isLoveTriangle) {
+                const loveTriangleId = [i, j, k].sort().join('|');
+                if (foundLoveTriangles.indexOf(loveTriangleId) === -1) {
+                  foundLoveTriangles.push(loveTriangleId);
+                  preferences[i] = null;
+                  preferences[j] = null;
+                  preferences[k] = null;
+                }
+
+              }
+
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return foundLoveTriangles.length;
 };
